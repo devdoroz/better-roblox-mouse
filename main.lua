94cf913f32eba42f8db51ceaa7aad12eb786715b
doroz <125635258+devdoroz@users.noreply.github.com>
2023-05-20 18:40:01 -0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,25 @@
+local player = game.Players.LocalPlayer
+local roblox_mouse = player:GetMouse()
+local runService = game:GetService("RunService")
+local camera = workspace.CurrentCamera
+
+roblox_mouse.TargetFilter = workspace
+
+local Mouse = {raycastParams = RaycastParams, Hit = CFrame.new(0, 0, 0), Target = nil}; do
+	function Mouse:SetRaycastParams(raycastParams: RaycastParams)
+		Mouse.raycastParams = raycastParams
+	end
+	
+	task.spawn(function()
+		while true do
+			runService.RenderStepped:Wait()
+			local hit = roblox_mouse.Hit.Position
+			local ray = workspace:Raycast(camera.CFrame.Position, (hit - camera.CFrame.Position) * 5000, Mouse.raycastParams)
+			local rayPos = (ray and ray.Position) or hit
+			Mouse.Hit = CFrame.new(ray.Position)
+			Mouse.Target = (ray and ray.Instance) or nil
+		end
+	end)
+end
+
+return Mouse
